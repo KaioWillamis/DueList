c6028b9dfea247f7c2b93ef3eb49a7c785d7301d
Kaio Willamis <gsasuke642@gmail.com>
2025-06-20 18:54:14 -0300

    Atualizei

---
+++ Back-end/Edition.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+# Carrega o CSV
+df = pd.read_csv("Banco/Dados.csv")
+
+print(df)
---
+++ Front-end/Principal.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
+
+janela = tk.Tk()
+janela.title("Minha Janela")
+janela.geometry("1280x720")
+
+janela.mainloop()
