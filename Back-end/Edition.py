55bf6cc830340f937d2c752848da58c8853623e6
Kaio Willamis <gsasuke642@gmail.com>
2025-06-27 17:53:30 -0300

    Essa é a forma certa

--- Back-end/Edition.py
+++ Back-end/Edition.py
@@ -1,6 +1,36 @@
 import pandas as pd
+import random
+import os
 
-# Carrega o CSV
-df = pd.read_csv("Banco/Dados.csv")
+CAMINHO_CSV = "Banco/Dados.csv"
 
-print(df)
+# Função para gerar um ID aleatório de 10 dígitos
+def gerar_id():
+    return str(random.randint(10**9, 10**10 - 1))
+
+def cadastrarCliente(nome, saldo):
+    id_novo = gerar_id()
+    #Função para verificar se o id que gerou já existe, se existir ele vai criar novamente
+    #até criar um que não existe
+    while id_novo in df["id"].astype(str).values:
+        id_novo = gerar_id()
+    
+    df = pd.read_csv(CAMINHO_CSV)
+    
+    novo_cliente = {
+        "id": id_novo,
+        "nome": nome,
+        "saldo": saldo
+    }
+
+    df = pd.concat([df, pd.DataFrame([novo_cliente])], ignore_index=True)
+    df.to_csv(CAMINHO_CSV, index=False)
+
+# Teste
+cadastrarCliente("Manoel", 1000)
+    
+    
+
+
+
+
